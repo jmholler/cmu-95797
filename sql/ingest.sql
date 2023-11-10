bb5bd49a03cde46a4ef026ca26a100ec1c0778f7
Joe Holleran <jmholler@andrew.cmu.edu>
2023-11-10 13:19:05 -0500

    updating repo between week-2 and week-3

--- sql/ingest.sql
+++ sql/ingest.sql
@@ -1,27 +1,27 @@
 -- create bike_data table with headers and filename column
-create table bike_data as select * from
-read_csv_auto('./data/citibike-tripdata*.csv.gz', union_by_name=True, filename=True, header=True);
+create or replace table bike_data as select * from
+read_csv_auto('./data/citibike-tripdata.csv.gz', union_by_name=True, filename=True, all_varchar=1);
 
 -- create central_park_weather table with headers and filename column
-create table central_park_weather as select * from
-read_csv_auto('./data/central_park_weather*.csv', union_by_name=True, filename=True, header=True);
+create or replace table central_park_weather as select * from
+read_csv_auto('./data/central_park_weather.csv', union_by_name=True, filename=True, all_varchar=1);
 
 -- create fhv_bases table with headers and filename column
-create table fhv_bases as select * from
-read_csv_auto('./data/fhv_bases*.csv', union_by_name=True, filename=True, header=True);
+create or replace table fhv_bases as select * from
+read_csv_auto('./data/fhv_bases.csv', union_by_name=True, filename=True, all_varchar=1, header=True);
 
 -- create fhv_tripdata table with filename column
-create table fhv_tripdata as select * from
-read_parquet('./data/taxi/fhv_tripdata*.parquet', union_by_name=True, filename=True);
+create or replace table fhv_tripdata as select * from
+read_parquet('./data/taxi/fhv_tripdata.parquet', union_by_name=True, filename=True);
 
 -- create fhvhv_tripdata table with filename column
-create table fhvhv_tripdata as select * from
-read_parquet('./data/taxi/fhvhv_tripdata*.parquet', union_by_name=True, filename=True);
+create or replace table fhvhv_tripdata as select * from
+read_parquet('./data/taxi/fhvhv_tripdata.parquet', union_by_name=True, filename=True);
 
 -- create green_tripdata table with filename column
-create table green_tripdata as select * from
-read_parquet('./data/taxi/green_tripdata*.parquet', union_by_name=True, filename=True);
+create or replace table green_tripdata as select * from
+read_parquet('./data/taxi/green_tripdata.parquet', union_by_name=True, filename=True);
 
 -- create yellow_tripdata table with filename column
-create table yellow_tripdata as select * from
-read_parquet('./data/taxi/yellow_tripdata*.parquet', union_by_name=True, filename=True);
+create or replace table yellow_tripdata as select * from
+read_parquet('./data/taxi/yellow_tripdata.parquet', union_by_name=True, filename=True);
