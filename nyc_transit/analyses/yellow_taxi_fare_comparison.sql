857bcec6a248b05bff580481a5851df5f38d892d
Joe Holleran <jmholler@andrew.cmu.edu>
2023-12-03 14:24:20 -0500

    week 5 BASIC WINDOW FUNCTIONS

---
+++ nyc_transit/analyses/yellow_taxi_fare_comparison.sql
@@ -0,0 +1,10 @@
+SELECT
+   yel.fare_amount,
+   AVG(yel.fare_amount) OVER() as avg_fare,
+   AVG(yel.fare_amount) OVER(PARTITION BY loc.Borough) as avg_borough_fare,
+   AVG(yel.fare_amount) OVER(PARTITION BY loc.Zone) as avg_zone_fare
+FROM {{ ref('stg__yellow_tripdata') }} yel
+JOIN {{ ref('mart__dim_locations') }} loc ON yel.PUlocationID = loc.LocationID
+-- I am limiting the output for filesize
+LIMIT 100000
+
